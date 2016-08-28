b9f2c2f0b038e46196e4386dc7939b40daa61455
elenacaseyroby <elenacaseyroby@gmail.com>
2016-08-27 18:52:17 -0700

    added sqlalchemy session connection to init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 #!flask/bin/python
 from flask import Flask
+from app import models
+from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
 
-from app import chatview, models
+Session = sessionmaker(bind=models.engine)
+Session.configure(bind=models.engine) 
+sql_session = Session()
+
+from app import chatview
 

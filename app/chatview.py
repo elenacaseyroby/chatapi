907be07a5891055e84560248dfc75444d3799101
elenacaseyroby <elenacaseyroby@gmail.com>
2016-08-28 18:35:55 -0700

    renamed api URIs to be more clear, getmessagethread() pulls threads by usernames now, postmessagethread() returns thread with new message every time a message is posted, adduser() adds a user. error messages are a work in progress.

--- app/chatview.py
+++ app/chatview.py
@@ -1,58 +1,82 @@
 from flask import render_template, redirect, request, Flask, jsonify
-from app import app, models
+from app import app, chatmodel, models
 from sqlalchemy import text, update, func
 from json import loads
 
-@app.route('/api/messages', methods = ['GET'])
-def getmessages():
+@app.route('/api/MessageThread', methods = ['GET']) 
+def getmessagethread():
 	messages = []
+	
 	if request.args.get('user1'):
 		user1 = request.args.get('user1')
-		#if 2 users specified, then get specific convo
 		if request.args.get('user2'):
 			user2 = request.args.get('user2')
-			where = "WHERE (messages.from_user = "+user1+" AND messages.to_user = "+user2+") OR (messages.from_user = "+user2+" AND messages.to_user = "+user1+")"
-		#else get all convos for one user
+			results = chatmodel.getmessagesbyuser(user1, user2)
 		else:
-			where = "WHERE messages.from_user = "+user1+" OR messages.to_user = "+user1
-
-		sql = text("""SELECT messages.time_sent
-			, messages.body
-			, u.username as `from`
-			, u2.username as `to`
-			FROM messages
-			JOIN users u ON messages.from_user = u.id
-			JOIN users u2 ON messages.to_user = u2.id
-			"""+where+"""
-			ORDER BY messages.time_sent;""")
-		results = models.engine.execute(sql)
+			results = chatmodel.getmessagesbyuser(user1)
 		for result in results:
-			message = { 'time': result[0]
-				, 'from': result[2]
-				, 'to': result[3]
-				, 'body': result[1]
+			message = { 'thread': result[7]
+				, 'message_id': result[0]
+				, 'time': result[4]
+				, 'from': result[5]
+				, 'to': result[6]
+				, 'body': result[3]
 				}
 			messages.append(message)
 
 	return jsonify(messages)
+#curl -i "http://localhost:5000/api/MessageThread?user1=[username1]"
+#curl -i "http://localhost:5000/api/MessageThread?user1=[username1]&user2=[username2]"
 
-@app.route('/api/messages', methods = ['POST'])
-def postmessages():
-    return "success"
+@app.route('/api/MessageThread', methods = ['POST'])
+def postmessagethread():
+	messages = []
+	if request.json['from'] and request.json['to'] and request.json['body']:
+		from_user = request.json['from']
+		to_user = request.json['to']
+		body = request.json['body']
+		post = chatmodel.postmessage(from_user = from_user, to_user = to_user, body = body)
+		results = chatmodel.getmessagesbyuser(from_user, to_user)
+		for result in results:
+			message = { 'message_id': result[0]
+				, 'time': result[4]
+				, 'from': result[5]
+				, 'to': result[6]
+				, 'body': result[3]
+				}
+			messages.append(message)
 
-@app.route('/api/messages', methods = ['PUT'])
-def updatemessages():
+		if post == "success":
+			return jsonify(messages)
+		else:
+			return "Check that you are using valid usernames."
+	else:
+		return "Check parameters."
+#curl -i -H "Content-Type: application/json" -X POST -d '{"from":"[username]", "to": "[username]", "body": "[body text]"}' http://localhost:5000/api/MessageThread
 
-    return "success"
-
-@app.route('/api/messages', methods = ['DELETE'])
-def deletemessages():
-
-    return "success"
-
-@app.route('/api/user', methods = ['POST'])
+@app.route('/api/User', methods = ['POST'])
 def adduser():
+	return_message = { 'error': 'Check params'}
+	if request.args.get('username') and request.args.get('email') and request.args.get('password'):
+		if '@' not in request.args.get('email'):
+			return_message = { 'error': 'Check that email is valid'}
+		elif ' ' in request.args.get('username'):
+			return_message = { 'error': 'Make sure there are no spaces in your username'}
+		elif ' ' in request.args.get('password'):
+			return_message = { 'error': 'Make sure there are no spaces in your password'}
+		else:
+			username = request.args.get('username')
+			email = request.args.get('email')
+			password = request.args.get('password')
+			post = chatmodel.addnewuser(username = username, email = email, password = password)
+			if post == "success":
+				return_message = {'success': 'user successfully created'}
 
-    return "success"
+	return return_message
+#curl -X POST "http://localhost:5000/api/User?username=[username]&email=[email]&password=[password]
+
+
+
+
 
 

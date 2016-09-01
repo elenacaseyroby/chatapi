b12f0c6facf916d8b0b899f7220d4591392f1c86
elenacaseyroby <elenacaseyroby@gmail.com>
2016-09-01 11:43:21 -0700

    test

--- run.py
+++ run.py
@@ -3,9 +3,9 @@ from app import app
 import os
 
 if 'HEROKU_CHECK' in os.environ:
-	app.run(host='0.0.0.0', port=int(os.environ.get("PORT"))) #imports app var from the app package 
-						#we made and calls its run method to start server.  
-						#since app var is Flask instance, we didn't have to 
-						#write the run method, it came for free.
+	print("heroku check")
+	app.run(host='0.0.0.0', port=int(os.environ.get("PORT"))) 
+
 else:
+	print("local")
 	app.run(debug=True)

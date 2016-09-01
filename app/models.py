0f8e46577de42e52239a07676426a25d83b27266
elenacaseyroby <elenacaseyroby@gmail.com>
2016-08-31 22:53:31 -0700

    should use heroku’s database on prod now

--- app/models.py
+++ app/models.py
@@ -5,12 +5,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref 
 import os
-"""
+
 if 'MYSQL_DATABASE_URL' in os.environ and os.environ['MYSQL_DATABASE_URL']:
     db_url = os.environ['MYSQL_DATABASE_URL']
 else:
-"""
-db_url = 'mysql+pymysql://root:@127.0.0.1:3306/chatdb'
+	db_url = 'mysql+pymysql://root:@127.0.0.1:3306/chatdb'
 
 engine = create_engine(db_url+'?charset=utf8', convert_unicode=True, echo=False)
 

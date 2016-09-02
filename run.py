f5811d8513040d3ae91f5d79ee8b8d4b05566ce6
elenacaseyroby <elenacaseyroby@gmail.com>
2016-09-02 16:24:58 -0700

    to test

--- run.py
+++ run.py
@@ -3,6 +3,6 @@ from app import app
 import os
 
 if 'heroku' in os.environ.get("PATH"):
-		app.run(host='0.0.0.0', port=int(os.environ.get("PORT"))) 
+		app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT"))) 
 else:
 	app.run(debug=True)

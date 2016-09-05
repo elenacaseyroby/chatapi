5ed804b4ea9ec00210f2f4f19a2bb9991d99cf86
elenacaseyroby <elenacaseyroby@gmail.com>
2016-09-04 19:10:47 -0700

    test bug fix - should rollback and try again if sql query fails

--- app/chatmodel.py
+++ app/chatmodel.py
@@ -2,7 +2,7 @@
 # -*- mode: python -*-
 
 from app import models, sql_session
-from sqlalchemy import text, update, func
+from sqlalchemy import text, update, func, exc
 from flask import session, request
 import datetime
 
@@ -74,13 +74,30 @@ def getallusers(user = None):
 
 def postmessage(from_user, to_user, body):
 	#check for users
-	from_user_in_db = sql_session.query(models.User).filter_by(username = from_user).first()
+	try:
+		from_user_in_db = sql_session.query(models.User).filter_by(username = from_user).first()
+	except exc.DBAPIError:
+		sql_session.rollback()
+		from_user_in_db = sql_session.query(models.User).filter_by(username = from_user).first()
+
 	if from_user_in_db:
 		from_user_id = from_user_in_db.id
-	to_user_in_db = sql_session.query(models.User).filter_by(username = to_user).first()
-	if to_user_in_db:
-		to_user_id = to_user_in_db.id
+		try:
+			to_user_in_db = sql_session.query(models.User).filter_by(username = to_user).first()
+		except exc.DBAPIError:
+			sql_session.rollback()
+			to_user_in_db = sql_session.query(models.User).filter_by(username = to_user).first()
+		if to_user_in_db:
+			to_user_id = to_user_in_db.id
+
 	if from_user_in_db and to_user_in_db:
+		try:
+			new_message = models.Message(from_user = int(from_user_id), to_user = int(to_user_id), body = str(body))
+			sql_session.add(new_message)
+			sql_session.commit()
+			return "success"
+		except exc.DBAPIError:
+			sql_session.rollback()
 			new_message = models.Message(from_user = int(from_user_id), to_user = int(to_user_id), body = str(body))
 			sql_session.add(new_message)
 			sql_session.commit()
@@ -89,15 +106,26 @@ def postmessage(from_user, to_user, body):
 		return "failure"
 
 def addnewuser(username, email, password):
-	username_in_db = sql_session.query(models.User).filter_by(username = username).first()
+	try:
+		username_in_db = sql_session.query(models.User).filter_by(username = username).first()
+	except exc.DBAPIError:
+		sql_session.rollback()
+		username_in_db = sql_session.query(models.User).filter_by(username = username).first()
+
 	if username_in_db:
 		return "failure"
 	else:
-		new_user = models.User(username = username, email = email, password = password)
-		sql_session.add(new_user)
-		sql_session.commit()
-		return "success"
-
+		try:
+			new_user = models.User(username = username, email = email, password = password)
+			sql_session.add(new_user)
+			sql_session.commit()
+			return "success"
+		except exc.DBAPIError:
+			sql_session.rollback()
+			new_user = models.User(username = username, email = email, password = password)
+			sql_session.add(new_user)
+			sql_session.commit()
+			return "success"
 
 
 
